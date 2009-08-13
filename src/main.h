2749888d83d59628372f10c967a8408f05fc00aa
gscholz <gscholz>
2009-08-13 20:20:00 +0000

    resource file renamed

--- src/main.h
+++ src/main.h
@@ -1,13 +1,11 @@
 #ifndef __MAIN_H
 #define __MAIN_H
 
-#define QMIDIARP_VERSION "0.3.0"
-
 #define MAX_PORTS 64
 #define SEQPOOL 2048
 #define MAXNOTES 128
 #define TICKS_PER_QUARTER 192
 #define MAXCHORD  33
-#define QMARCNAME ".qmarc"
+#define QMARCNAME ".qmidiarprc"
 
 #endif
