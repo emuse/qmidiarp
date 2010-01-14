0568e86cb2035d7b3bff53c7a3f5bb6e82dc46fb
emuse <emuse>
2010-01-14 18:05:26 +0000

    some members moved to private

--- src/midiarp.h
+++ src/midiarp.h
@@ -33,6 +33,11 @@ class MidiArp : public QObject  {
     void initLoop();  
     int clip(int value, int min, int max, bool *outOfRange);
     int sustain;
+    int randomVelocity, randomTick, randomLength;
+    int octave, noteIndex[MAXCHORD], patternIndex;
+    int notes[2][4][MAXNOTES]; // Buffer Index, Note/Velocity/On-offTick/releaseMark, Data Index
+    double old_attackfn[MAXNOTES];
+    int noteBufPtr, noteCount, patternLen, patternMaxIndex, noteOfs;
     
   public:
     int chIn;       // Channel of input events
@@ -40,15 +45,10 @@ class MidiArp : public QObject  {
     int rangeIn[2]; // Parameter that is mapped, [0] low, [1] high boundary
     int portOut;    // Output port (ALSA Sequencer)
     int channelOut;
-    int notes[2][4][MAXNOTES]; // Buffer Index, Note/Velocity/On-offTick/releaseMark, Data Index
-    double old_attackfn[MAXNOTES];
-    int noteBufPtr, noteCount, patternLen, patternMaxIndex, noteOfs;
     bool hold, isMuted;
     int repeatPatternThroughChord;
     double tempo, len, vel;
     double attack_time, release_time; 
-    int octave, noteIndex[MAXCHORD], patternIndex;
-    int randomVelocity, randomTick, randomLength;
     int randomTickAmp, randomVelocityAmp, randomLengthAmp;
     QString pattern;
            
