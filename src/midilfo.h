52d1a62376dab6d9cea2ec4785aff40c6fa3e657
emuse <emuse@users.sourceforge.net>
2011-01-05 16:01:52 +0100

    - more occurences of support address updated

--- src/midilfo.h
+++ src/midilfo.h
@@ -1,7 +1,7 @@
 /*
  *      midilfo.h
  *      
- *      Copyright 2009 <alsamodular-devel@lists.sourceforge.net>
+ *      Copyright 2009 <qmidiarp-devel@lists.sourceforge.net>
  *      
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
