16c5a9c46a3c8b32185a5a05733e1622f0857a2b
emuse <emuse@users.sourceforge.net>
2011-01-05 16:05:58 +0100

    - more support address updates

--- src/midicctable.h
+++ src/midicctable.h
@@ -1,7 +1,7 @@
 /*
  *      midicctable.h
  *      
- *      Copyright 2009 <alsamodular-devel@lists.sourceforge.net>
+ *      Copyright 2009, 2010, 2011 <qmidiarp-devel@lists.sourceforge.net>
  *      
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
--- src/seqwidget.cpp
+++ src/seqwidget.cpp
@@ -1,7 +1,7 @@
 /*
  *      seqwidget.cpp
  *      
- *      Copyright 2009 <qmidiarp-devel@lists.sourceforge.net>
+ *      Copyright 2009, 2010, 2011 <qmidiarp-devel@lists.sourceforge.net>
  *      
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
