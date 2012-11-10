3cb267224420c18cfb06109d7680e4e90198ea93
emuse <emuse@users.sourceforge.net>
2012-11-10 12:21:27 +0100

    Documentation updated for some functions

--- src/parstore.h
+++ src/parstore.h
@@ -123,18 +123,24 @@ class ParStore : public QWidget
     QList<TempStore> list; /**< List of TempStore::temp structures for
                         parameter storage*/
 
-    int restoreRequest;
-    int oldRestoreRequest;
-    bool restoreRunOnce;
-    bool needsGUIUpdate;
-    int dispReqIx, dispReqSelected;
+    int restoreRequest; /**< When this variable is greater than -1,
+                        ParStore::updateDisplay() will cause
+                        ParStore::restoreParams(restoreRequest) at pattern
+                        end*/
+    int oldRestoreRequest; /**< Contains the last active location for jumping back*/
+    bool restoreRunOnce; /**< Signals to ParStore::updateDisplay() that only one
+                        run is done and then a restore is required */
+    bool needsGUIUpdate; /**< Signals to ParStore::updateDisplay() that the location
+                        button color has to be updated*/
+    int dispReqIx, dispReqSelected; /**< When ParStore::needsGUIUpdate is true,
+                        ParStore::updateDisplay() sets colors to these values*/
 
 /*!
 * @brief stores all module parameters to TempStore::temp and stores temp
 * in TempStore::list at index ix. If the given index is greater than the list size,
 * temp is appended to TempStore::list.
 *
-* @param ix index at which the parameters are stored.
+* @param ix Index at which the parameters are stored.
 */
     void tempToList(int ix);
 /*!
@@ -152,19 +158,84 @@ class ParStore : public QWidget
 */
     void writeData(QXmlStreamWriter& xml);
 
+/*!
+* @brief sets ParStore::restoreRequest and ParStore::restoreRunOnce to the
+* location specified
+*
+* @param ix Location index to be restored at pattern end
+*/
     void setRestoreRequest(int ix);
+/*!
+* @brief sets the color of location button row to the specified color index
+*
+* @param row Location index at which button color is to be set
+* @param color Location color (0: no color, 1: active color, 2: pending color)
+*/
     void setBGColorAt(int row, int color);
+/*!
+* @brief is called by the parent widget and part of the display timer event loop.
+
+* sets the indicator position and handles storage requests as a function of the
+* GUI requests and series parameters
+*
+* @param frame Current frame position of the parent module
+* @param reverse Set to true if the parent module currently plays backward
+*/
     void updateDisplay(int frame, bool reverse);
 
   signals:
+/*!
+* @brief connected to the parent widget and causing parameter storage to the
+* location ix
+*
+* @param ix Storage location
+* @param empty True if no parameters are stored and only the template is added
+*/
     void store(int ix, bool empty);
+/*!
+* @brief connected to the parent widget and causing immediate parameter restore from
+* location ix
+*
+* @param ix Storage location
+*/
     void restore(int ix);
 
   public slots:
 
+/*!
+* @brief a signal mapper for routing the ParStore::jumpToIndexMenu signals
+* to the ParStore::updateRunOnce() slot
+*
+* @param *action Pointer to the action that has been activated in the menu
+*/
     void mapJumpToGroup(QAction *action);
+/*!
+* @brief called by ParStore::mapJumpToGroup(). Configures the location
+* behavior at pattern end
+*
+* The choices are -2 for "Stay here" (no jumps at pattern end), -1 for
+* returning to the previous location (ParStore::oldRestoreRequest) or (if
+* zero or above) the location to jump to at pattern end.
+*
+* @param location Location to be configured
+* @param choice -2 (no jumps), -1 (return to previous),
+* >=0 (next location to jump to)
+*/
     void updateRunOnce(int location, int choice);
+/*!
+* @brief adds all storage location GUI elements to the widget
+*
+* It also adds a new element to the ParStore::jumpToList
+*/
     void addLocation();
+/*!
+* @brief removes and deletes all storage location GUI elements
+*
+* It also removes the parameter storage structure from ParStore::list and
+* the element from ParStore::jumpToList
+*
+* @param ix Location index to be removed
+*/
     void removeLocation(int ix);
     void mapRestoreSignal();
     void mapStoreSignal();
